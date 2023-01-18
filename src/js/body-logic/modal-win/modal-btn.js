6b106b3ef22d3589052ba704ddcaee3aad78ca22
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2023-01-18 22:16:37 +0200

    123

--- src/js/body-logic/modal-win/modal-btn.js
+++ src/js/body-logic/modal-win/modal-btn.js
@@ -1,6 +1,5 @@
 // import { showWatchedFilms ,showWQueueFilms} from '../../lib-js/render-lib';
 
-
 const resFromWatch = getIlocaltemWatchedme();
 const resFromQueue = getlocalItemQueue();
 
@@ -16,24 +15,22 @@ if (resFromQueue) {
 export function btnWatched(modalWatched, queryText) {
   let idCard = localStorage.getItem('data');
   if (queryText === 'remove from Watched') {
-
     if (arrIdCardForWatched.length === 1) {
       arrIdCardForWatched = [];
       modalWatched.textContent = 'add to Watched';
       return;
     }
-   for (let i = 0; i < resFromWatch.length; i++) {
-     if (resFromWatch[i] === idCard) {
-       resFromWatch.splice(i, 1);
-       modalWatched.textContent = 'Add to Watched';
-     }
-   }
-   return;
+    for (let i = 0; i < resFromWatch.length; i++) {
+      if (resFromWatch[i] === idCard) {
+        resFromWatch.splice(i, 1);
+        modalWatched.textContent = 'Add to Watched';
+      }
+    }
+    return;
   }
   modalWatched.textContent = 'remove from Watched';
   modalWatched.style.padding = '6px';
   arrIdCardForWatched.push(idCard);
-  
 }
 
 export function btnQueue(modalQueue, queryText) {
@@ -46,20 +43,19 @@ export function btnQueue(modalQueue, queryText) {
       modalQueue.style.paddingRight = '16px';
       modalQueue.style.paddingLeft = '16px';
       return;
-      }
+    }
     for (let i = 0; i < resFromQueue.length; i++) {
       if (resFromQueue[i] === idCard) {
-        resFromQueue.splice(i,1);
+        resFromQueue.splice(i, 1);
         modalQueue.textContent = 'Add to queue';
       }
-      }
- 
-      return
+    }
+
+    return;
   }
   modalQueue.textContent = 'remove from queue';
   modalQueue.style.padding = '6px';
   arrIdCardForQueue.push(idCard);
-
 }
 
 function getIlocaltemWatchedme() {
--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,3 @@
 import './js/body-logic/modal-win/modal';
 import './js/body-logic/modal-win/modal-btn';
 import './js/lib-js/render-lib';
-// import {
-//   renderCardsByTrend,
-//   renderCardsBySearch,
-// } from './js/body-logic/render-cards';
-// renderCardsByTrend();
